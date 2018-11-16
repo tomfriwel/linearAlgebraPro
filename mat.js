89d42e3f1822c58094bb13359491830795f29671
tomfriwel <976932447@qq.com>
2018-11-16 14:46:28 +0800

    add, subtract operation

--- mat.js
+++ mat.js
@@ -1,5 +1,4 @@
 // matrix object
-
 class Mat {
     constructor(array) {
         console.log(array);
@@ -29,5 +28,32 @@ class Mat {
         }
 
         return new Mat(newArray);
+    }
+
+    // subtract operation
+    static subtract(mat0, mat1) {
+        if (mat0.rowLength != mat1.rowLength && mat0.columnLength != mat1.columnLength) {
+            throw new Error("只有同型矩阵才能进行加法运算");
+        }
+
+        let newArray = new Array(mat0.columnLength);
+        for (let i = 0; i < mat0.columnLength; i++) {
+            if(!newArray[i]) {
+                newArray[i] = new Array(mat0.rowLength);
+            }
+            for (let j = 0; j < mat0.rowLength; j++) {
+                newArray[i][j] = mat0.array[i][j] - mat1.array[i][j];
+            }
+        }
+
+        return new Mat(newArray);
+    }
+
+    add(mat) {
+        return Mat.add(this, mat)
+    }
+
+    subtract(mat) {
+        return Mat.subtract(this, mat)
     }
 }
